b368f3c13e54852a0e18fdd748649cab0dcfd88e
Robin Cornelius <robin.cornelius@gmail.com>
2017-05-19 09:21:25 +0100

    Add in simple test projects for driver loader and libCanopenSimple Add an overall solution Improve gitignore

---
+++ DriverTest/DriverTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using libCanopenSimple;
+
+namespace DriverTest
+{
+    /// <summary>
+    /// This demonstrates how to load a can festival driver, open the channel and recieve data
+    /// </summary>
+    class DriverTest
+    {
+        static void Main(string[] args)
+        {
+
+            //Change these to load correct driver and connect it to correct bus  
+            string driver = "can_usb_win32";
+            string bus = "COM4";
+            BUSSPEED bitrate = BUSSPEED.BUS_500Kbit;
+
+
+            try
+            {
+                DriverLoader loader = new DriverLoader();
+                DriverInstance instance = loader.loaddriver(driver);
+
+                Console.WriteLine("Opening CAN device ");
+                instance.open(bus, bitrate);
+
+                instance.rxmessage += Instance_rxmessage;
+                Console.WriteLine("listening for any traffic, press any key to exit ..");
+                while (!Console.KeyAvailable)
+                {
+
+                }
+
+                instance.close();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("That did not work out, exception message was \n" + e.ToString());
+            }
+
+            Console.WriteLine("Press any key to exit");
+
+            while (!Console.KeyAvailable)
+            {
+            }
+        }
+
+        private static void Instance_rxmessage(DriverInstance.Message msg)
+        {
+            canpacket cp = new canpacket(msg);
+            Console.WriteLine("RX :" + cp.ToString());
+        }
+    }
+}
---
+++ SimpleTest/SimpleTest.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using libCanopenSimple;
+
+namespace SimpleTest
+{
+    class SimpleTest
+    {
+
+        static void Main(string[] args)
+        {
+
+            //Change these to load correct driver and connect it to correct bus  
+            string driver = "can_usb_win32";
+            string bus = "COM4";
+            BUSSPEED bitrate = BUSSPEED.BUS_500Kbit;
+
+            try
+            {
+                libCanopenSimple.libCanopenSimple lco = new libCanopenSimple.libCanopenSimple();
+
+                lco.nmtevent += Lco_nmtevent;
+                lco.nmtecevent += Lco_nmtecevent;
+                lco.pdoevent += Lco_pdoevent;
+                lco.sdoevent += Lco_sdoevent;
+
+                lco.open(4, bitrate, driver);
+
+                Console.WriteLine("listening for any traffic");
+
+                Console.WriteLine("Sending NMT reset all nodes in 5 seconds");
+
+                System.Threading.Thread.Sleep(5000);
+
+                lco.NMT_ResetNode(); //reset all
+
+                Console.WriteLine("Press any key to exit test..");
+
+                while (!Console.KeyAvailable)
+                {
+
+                }
+
+                lco.close();
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("That did not work out, exception message was \n" + e.ToString());
+            }
+
+
+        }
+
+        private static void Lco_nmtecevent(canpacket p)
+        {
+            Console.WriteLine("NMTEC :" + p.ToString());
+        }
+
+        private static void Lco_sdoevent(libCanopenSimple.canpacket p)
+        {
+            Console.WriteLine("SDO :" + p.ToString());
+        }
+
+        private static void Lco_pdoevent(libCanopenSimple.canpacket[] ps)
+        {
+            foreach(canpacket p in ps)
+                Console.WriteLine("PDO :" + p.ToString());
+        }
+
+        private static void Lco_nmtevent(libCanopenSimple.canpacket p)
+        {
+            Console.WriteLine("NMT :" + p.ToString());
+        }
+    }
+}
